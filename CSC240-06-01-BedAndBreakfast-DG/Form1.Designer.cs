4eede44388db2a3a2cf3f969832ac3490eabbd94
Dixon Geiser <DGEISE2490@my.sullivan.edu>
2024-08-04 22:23:05 -0400

    Beginning of third form (Lincoln Room)

--- CSC240-06-01-BedAndBreakfast-DG/Form1.Designer.cs
+++ CSC240-06-01-BedAndBreakfast-DG/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             UxWelcomeLabel = new Label();
             UxRateLabel = new Label();
+            belleaireCheckBox = new CheckBox();
+            lincolnCheckBox = new CheckBox();
             SuspendLayout();
             // 
             // UxWelcomeLabel
@@ -45,18 +47,45 @@
             // UxRateLabel
             // 
             UxRateLabel.AutoSize = true;
-            UxRateLabel.Location = new Point(177, 134);
+            UxRateLabel.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            UxRateLabel.Location = new Point(139, 130);
             UxRateLabel.Name = "UxRateLabel";
-            UxRateLabel.Size = new Size(89, 15);
+            UxRateLabel.Size = new Size(164, 25);
             UxRateLabel.TabIndex = 1;
             UxRateLabel.Text = "Check our rates";
+            // 
+            // belleaireCheckBox
+            // 
+            belleaireCheckBox.AutoSize = true;
+            belleaireCheckBox.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            belleaireCheckBox.Location = new Point(139, 199);
+            belleaireCheckBox.Name = "belleaireCheckBox";
+            belleaireCheckBox.Size = new Size(173, 28);
+            belleaireCheckBox.TabIndex = 2;
+            belleaireCheckBox.Text = "Belle Aire Suite";
+            belleaireCheckBox.UseVisualStyleBackColor = true;
+            belleaireCheckBox.CheckedChanged += belleaireCheckBox_CheckedChanged;
+            // 
+            // lincolnCheckBox
+            // 
+            lincolnCheckBox.AutoSize = true;
+            lincolnCheckBox.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lincolnCheckBox.Location = new Point(139, 273);
+            lincolnCheckBox.Name = "lincolnCheckBox";
+            lincolnCheckBox.Size = new Size(158, 28);
+            lincolnCheckBox.TabIndex = 3;
+            lincolnCheckBox.Text = "Lincoln Room";
+            lincolnCheckBox.UseVisualStyleBackColor = true;
+            lincolnCheckBox.CheckedChanged += lincolnCheckBox_CheckedChanged;
             // 
             // UxBaileysForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Gold;
-            ClientSize = new Size(449, 450);
+            BackColor = Color.Yellow;
+            ClientSize = new Size(442, 395);
+            Controls.Add(lincolnCheckBox);
+            Controls.Add(belleaireCheckBox);
             Controls.Add(UxRateLabel);
             Controls.Add(UxWelcomeLabel);
             Name = "UxBaileysForm";
@@ -69,5 +98,7 @@
 
         private Label UxWelcomeLabel;
         private Label UxRateLabel;
+        private CheckBox belleaireCheckBox;
+        private CheckBox lincolnCheckBox;
     }
 }
